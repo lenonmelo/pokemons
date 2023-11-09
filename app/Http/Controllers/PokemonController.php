3491384f7d01061788d066a15be714ed72c88b9f
lenonmelo <32593846+lenonmelo@users.noreply.github.com>
2023-11-09 17:28:23 -0300

    Incluído acesso de permissão na classe PokemonsController

--- app/Http/Controllers/PokemonController.php
+++ app/Http/Controllers/PokemonController.php
@@ -10,6 +10,11 @@ use Illuminate\Http\Request;
 
 class PokemonController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
      /**
      * Mostra a lista de pokémons.
      *
